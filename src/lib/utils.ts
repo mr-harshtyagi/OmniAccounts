0d426fcce6be740cf49e61429c41860aae529143
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-09-07 13:27:45 +0530

    feat: add utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import {
+  abi,
+  sepoliaAddress,
+  arbitrumSepoliaAddress,
+  optimismSepoliaAddress,
+  baseSepoliaAddress,
+} from "@/lib/contracts/NFTWallet.json";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function chainIdToContractAddress(chainId: number | undefined) {
+  switch (chainId) {
+    case 111155111:
+      return sepoliaAddress;
+    case 421614:
+      return arbitrumSepoliaAddress;
+    case 11155420:
+      return optimismSepoliaAddress;
+    case 84532:
+      return baseSepoliaAddress;
+    default:
+      return sepoliaAddress;
+  }
 }
